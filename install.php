8b1fcd96401d3b298c45e91a4bf1c4fdb2547c69
kaipCS <parker.ceh@oundleschool.org.uk>
2025-05-15 12:25:46 +0100

    install users

--- install.php
+++ install.php
@@ -3,13 +3,13 @@ include_once("connection.php");
 $stmt = $conn->prepare("DROP TABLE IF EXISTS users;
 CREATE TABLE users 
 (userid INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-gender VARCHAR(1) NOT NULL,
-surname VARCHAR(20) NOT NULL,
-forename VARCHAR(20) NOT NULL,
-password VARCHAR(20) NOT NULL,
-house VARCHAR(20) NOT NULL,
-year INT(2) NOT NULL,
-role TINYINT(1))
+email VARCHAR(254) NOT NULL,
+forename VARCHAR(50) NOT NULL,
+surname VARCHAR(50) NOT NULL,
+role TINYINT(1) NOT NULL,
+schoolID INT(5),
+password VARCHAR(255) NOT NULL,
+UNIQUE (email)
 ");
 $stmt->execute();
 $stmt->closeCursor();
