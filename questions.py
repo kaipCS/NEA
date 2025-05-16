fb9befd93fccf6c864b96956590e5655fe50a425
kaipCS <parker.ceh@oundleschool.org.uk>
2025-05-16 10:32:31 +0100

    questions

--- questions.py
+++ questions.py
@@ -45,43 +45,35 @@ for question in questions[1:]:
         area = 1 
     else:
         area = 2
-    print(topic, year, paper, area)
 
     #url for tex file 
 
-    #spec case
+    #specimen case
     if year == 1986:
-        texurl = url + "/database/db/Spec/Spec-S" + str(paper) + ".tex"
+        texurl = url+"/database/db/Spec/Spec-S"+str(paper)+".tex"
     #otherwise
     else:
-        texurl = url + "/database/db/" + str(year)[2:] + "/" + str(year)[2:] + "-S" + str(paper) + ".tex"
-    print(texurl)
-
-
-# #convert question data json file into a list
-# questions = data if isinstance(data, list) else data.get("questions") or data.get("data") or []
-# print("questions")
-# print(questions)
-
-
-#import re
-#from urllib.parse import urljoin
-# # Process each question
-# for i, question in enumerate(questions):
-#     title = question.get("title", f"Question {i+1}")
-#     body = question.get("body", "")
+        texurl = url+"/database/db/"+str(year)[2:]+"/"+str(year)[2:]+"-S"+str(paper)+".tex"
     
-#     # Try to extract the .tex file link
-#     match = re.search(r'href="(/database/db/[^"]+\.tex)"', body)
-#     if match:
-#         tex_url = urljoin(BASE_URL, match.group(1))
-#         print("starting outputing")
-#         print(f"{i+1}. {title}")
-#         print(f"TeX URL: {tex_url}")
-        
-#         # Try to download and show the content (or first few lines)
-#         tex_resp = requests.get(tex_url, verify=False)
-#         if tex_resp.status_code == 200:
-#             lines = tex_resp.text.splitlines()
-#             for line in lines[:10]: # show first 10 lines 
-#                 print(line)
+    #load text from url
+    response = requests.get(texurl, verify = False)
+    texfile = response.text
+    
+    #split by questions
+    sections = texfile.split("begin{question}")
+    
+    #identify question code
+    # issue with 93-S2-Q7
+    if question["question_id"] == 93207:
+        questionnum = 7
+    else:
+        questionnum = int(title[-1])
+    code = sections[questionnum]
+
+    #clean code
+
+    index = code.find("end{question}")
+    code = code[:index-1]
+    
+    #all data 
+    print(topic, year, paper, area, code)
