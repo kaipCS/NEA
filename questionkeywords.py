625605bc529ffebb2c808e6f288030c5c1488cc3
kaipCS <parker.ceh@oundleschool.org.uk>
2025-05-20 09:38:28 +0100

    commit

---
+++ questionkeywords.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+#array to hold dictionaries
+
+#open file to store keywords
+file = open("questionkeywords.txt", "w")
+
+#array to hold dictionaries
+data = []
+
+#url of json file
+url = "https://stepdatabase.maths.org/database/database_data.json"
+
+#open file and extract questions 
+response = requests.get(url, verify=False)
+data = response.json()
+questions = data["questions"]
+ 
+#question ID counter
+counter =  0
+
+#iterate through each ignoring first start question
+for question in questions[1:]:
+    counter += 1
+    #extract section with only kewyords
+    information = question["body"]
+    array = information.split(".")
+    keywords = array[0]
+    #issue of some not having full stops
+    if "<p>" in keywords:
+        array = information.split("<p>")
+        keywords = array[0]
+    
+    #split list of keywords into an array of individual ones
+    keywordsarray = keywords.split(",")
+    info= {"questionid" : counter, "keywords" : keywords}
+    data.append(info)
+
+file.write(data)
+#print(keywordarray)
+file.close()
