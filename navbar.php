faf5ec469461979ee0153f6e0c2eb60d96fdd6de
kaipCS <parker.ceh@oundleschool.org.uk>
2025-06-03 17:40:50 +0100

    css

--- navbar.php
+++ navbar.php
@@ -1,8 +1,8 @@
 <nav class="navbar">
   <div class="container-fluid">
     <a class="logo" href="homepage.php">STEP</a>
-    <a href="#">QUESTIONS</a>
-    <a href="#">BUILD PAPER</a>
+    <a class="navbar-button" href="questionspage.php">QUESTIONS</a>
+    <a class = "navbar-button" href="builpaperpage.php">BUILD PAPER</a>
     <div class="dropdown">
       <button class="dropbtn">ACCOUNT</button>
       <div class="dropdown-content">
