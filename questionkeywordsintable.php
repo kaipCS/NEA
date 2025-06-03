c781ea2ac0e01a9bb34d8d44d2624269eff13524
kaipCS <parker.ceh@oundleschool.org.uk>
2025-06-03 09:13:16 +0100

    navbar

--- questionkeywordsintable.php
+++ questionkeywordsintable.php
@@ -6,17 +6,19 @@ $file = file_get_contents('questionkeywords.json');
 $items = json_decode($file, true);
 #iterate through each item in file 
 foreach ($items as $item) {
-#extract data from dictionary
-$keywords = $item["keywords"];
-$questionid = $item["questionid"];
-#insert into table
-$stmt = $conn->prepare("INSERT INTO questionhaskeyword (questionid, keyword) VALUES (:questionid, :keyword)");
-foreach ($keywords as $keyword){
-    $keyword = trim($keyword);
-    $keyword = strtolower($keyword);
-    $stmt->bindParam(":questionid", $questionid);
-    $stmt->bindParam(":keyword", $keyword);
-    $stmt->execute();
+    #extract data from dictionary
+    $keywords = $item["keywords"];
+    $questionid = $item["questionid"];
+    #insert into table
+    $stmt = $conn->prepare("INSERT INTO questionhaskeyword (questionid, keyword) VALUES (:questionid, :keyword)");
+    #iterate through keywords
+    foreach ($keywords as $keyword){
+        #consistent formatting on keywords
+        $keyword = trim($keyword);
+        $keyword = strtolower($keyword);
+        $stmt->bindParam(":questionid", $questionid);
+        $stmt->bindParam(":keyword", $keyword);
+        $stmt->execute();
 }}
 echo("added succesfully");
 ?>
