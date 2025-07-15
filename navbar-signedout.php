31d89f468ae629d9027c159ecd5d5ffdc7c64e42
kaipCS <parker.ceh@oundleschool.org.uk>
2025-07-15 14:45:31 +0100

    edit template

--- navbar-signedout.php
+++ navbar-signedout.php
@@ -1,8 +1,6 @@
 <nav class="navbar">
   <div class="container-fluid">
     <a id="logo" href="homepage.php">STEP</a>
-    <a class="navbar-button" href="questionspage.php">QUESTIONS</a>
-    <a class = "navbar-button" href="builpaperpage.php">BUILD PAPER</a>
     <a class = "navbar-button" id="signin" href="signin.php">SIGN IN</a>
   </div>
 </nav>
--- template.php
+++ template.php
@@ -3,7 +3,7 @@
 <head>
   <title>Template</title>
   <!-- Links to style sheet and bootstrap -->
-  <link rel="stylesheet" href="stylesheet.css">
+  <link rel="stylesheet" href="test.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -12,7 +12,7 @@
 <body>
 
 <!-- Link to include navbar -->
-<?php include 'navbar-signedout.php'; ?>
+<?php include 'test.php'; ?>
 
 <div class="container">
   <h1>Page contents</h1>
