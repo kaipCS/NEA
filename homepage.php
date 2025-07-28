ded018dc4ef8cdf97a94743e3120f87af44b5bda
kaipCS <parker.ceh@oundleschool.org.uk>
2025-07-28 13:39:59 +0100

    homepage text and bottom bar css

---
+++ homepage.php
@@ -0,0 +1,94 @@
+<?php session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Homepage</title>
+  <!-- Links to style sheet and bootstrap -->
+  <link rel="stylesheet" href="stylesheet.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<!-- Link to include appropriate navbar -->
+<?php 
+echo($_SESSION['userid']);
+if (isset($_SESSION['userid'])):
+  include 'navbar-signedin.php';
+else:
+  include 'navbar-signedout.php';
+endif; ?>
+
+<!-- Page contents -->
+<div class="container">
+
+  <!-- What is step and dates -->
+  <div class="row">
+    <div class="col-sm-8">
+    <h2>
+      What is STEP 
+    </h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    </div>
+    <div class="col-sm-4">
+    
+    <!--Countdown to STEP exams -->
+    <h3>  
+      Upcoming dates
+    </h3>
+    </div>
+  </div>
+
+  <!-- How to use website -->
+  <div class="row">
+    <div class="col-sm-6">
+      <h2>
+        How to use website
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+    <div class="col-sm-3">
+      <h3>
+        For teachers
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. 
+      </p>
+    </div>
+    <div class="col-sm-3">
+      <h3>
+        For students
+      </h3>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. 
+      </p>
+    </div>
+  </div>
+
+    <!-- How to prepare -->
+    <div class="row">
+    <div class="col-sm-12">
+      <h3>
+        Tips for preparation
+      </h3>
+      <p> 
+        Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  </div>
+</div>
+
+<!-- Bottom blue bar -->
+<div class="bottom-bar">
+<a> </a>
+</div>
+
+</body>
+</html>
