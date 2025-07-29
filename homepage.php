283d51c246708cc730f95b6ef2dab44566a28906
kaipCS <parker.ceh@oundleschool.org.uk>
2025-07-29 13:59:59 +0100

    how to use this website

--- homepage.php
+++ homepage.php
@@ -52,7 +52,7 @@ endif; ?>
   <div class="row">
     <div class="col-sm-6">
       <h2>
-        How to use website
+        How to use this website
       </h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
