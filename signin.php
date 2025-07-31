cbdc8c5fa1ca6b4002524ebf5da41b0f7b788e72
kaipCS <parker.ceh@oundleschool.org.uk>
2025-07-31 15:23:55 +0100

    error messages

--- signin.php
+++ signin.php
@@ -20,72 +20,71 @@ include 'navbar-signedout.php';
 
 <!-- Page contents -->
 <div id="sign-in" class="container">
-    <!-- Sign in box -->
-    <div class="col-sm-4">
-        <h3>
-            SIGN IN
-        </h3>
-    
-        <!-- Sign in form -->
-        <form action="signinuser.php"  method="POST">
-            Email<br>
-            <input type="text" id="email" name="email"><br>
-            Password<br>
-            <input type="password" id="password" name="password">
-            <br>
-            <div class="error-message">
-              <?php
-              print_r($_SESSION);
-              if ($_SESSION["error"] == "emptySignIn"){
-                echo "empty error message";
-              }
-              if ($_SESSION["error"] == "noUser"){
-                echo "user does not exist error message";
-              }
-              if ($_SESSION["error"] == "password"){
-                echo "password error message";
-              }
-              ?>
-            </div>
-            <br>
-            <input type="submit" value="Sign in">
-        </form>
-    </div>
-    
-    <!-- Create account box -->
-    <div class="col-sm-4">
-        <h3>
-            CREATE ACCOUNT    
-        </h3>
+  <!-- Sign in box -->
+  <div class="col-sm-4">
+      <h3>
+          SIGN IN
+      </h3>
+  
+      <!-- Sign in form -->
+      <form action="signinuser.php"  method="POST">
+          Email<br>
+          <input type="text" id="email" name="email"><br>
+          Password<br>
+          <input type="password" id="password" name="password">
+          <div class="error-message">
+            <?php
+            #print_r($_SESSION);
+            if ($_SESSION["error"] == "emptySignIn"){
+              echo "Please enter details into all fields.";
+            }
+            if ($_SESSION["error"] == "noUser"){
+              echo "There is no account associated with this email. If you have not signed up, create an account.";
+            }
+            if ($_SESSION["error"] == "password"){
+              echo "Unsuccessful login. Check your password and try again.";
+            }
+            ?>
+          </div>
+          <br>
+          <input type="submit" value="Sign in">
+      </form>
+  </div>
+  
+  <!-- Create account box -->
+  <div class="col-sm-4">
+      <h3>
+          CREATE ACCOUNT    
+      </h3>
 
-        <!-- Create account form -->
-        <form action="createaccount.php"  method="POST">
-            Forename<br>
-            <input type="text" id="forename" name="forename"><br>
-            Surname<br>
-            <input type="text" id="surname" name="surname"><br>
-            Email<br>
-            <input type="text" id="email" name="email"><br>
-            Password<br>
-            <input type="password" id="password" name="password"><br>
-            <input type="radio" name="role" value="Student" checked> Student
-            <input type="radio" name="role" value="Teacher"> Teacher
-            <br>
-            <div class= "error-message">
-              <?php
-              #print_r($_SESSION);
-              if ($_SESSION["error"] == "emptyAccount"){
-                echo "empty error message";
-              }
-              if ($_SESSION["error"] == "emailExists"){
-                echo "email error message";
-              }
-              ?>
-            </div>
-            <br>
-            <input type="submit" value="Create account">
-        </form>
-    </div>
+      <!-- Create account form -->
+      <form action="createaccount.php"  method="POST">
+          Forename<br>
+          <input type="text" id="forename" name="forename"><br>
+          Surname<br>
+          <input type="text" id="surname" name="surname"><br>
+          Email<br>
+          <input type="text" id="email" name="email"><br>
+          Password<br>
+          <input type="password" id="password" name="password"><br>
+          <input type="radio" name="role" value="Student" checked> Student
+          <input type="radio" name="role" value="Teacher"> Teacher
+          <br>
+          <div class= "error-message">
+            <?php
+            #print_r($_SESSION);
+            if ($_SESSION["error"] == "emptyAccount"){
+              echo "Please enter details into all fields.";
+            }
+            if ($_SESSION["error"] == "emailExists"){
+              echo "An account with this email already exists. Sign in to access your account.";
+            }
+            ?>
+          </div>
+          <br>
+          <input type="submit" value="Create account">
+      </form>
+  </div>
 </div>
 
 <!-- Bottom blue bar -->
