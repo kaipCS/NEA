315b6db88ed3c0980a41671a5e9da0dcdacdf8b3
kaipCS <parker.ceh@oundleschool.org.uk>
2025-08-02 14:51:36 +0100

    write up school

--- school.php
+++ school.php
@@ -4,18 +4,23 @@ session_start();
 # connect to database
 include_once("connection.php");
 
-#redirect to correct page
+#get userid for current user 
 $userid = $_SESSION["userid"];
+
+#get schoolID for the current user for database
 $stmt = $conn->prepare("SELECT schoolID FROM users WHERE userid = :userid");
 $stmt->bindParam(':userid', $userid);
 $stmt->execute();
 $school = $stmt->fetch(PDO::FETCH_ASSOC);
-if ($school && $school['schoolID'] !== null){
+#check if schoolID is null 
+if ($school['schoolID'] !== null){
+    #the user is in a school
     $_SESSION["schoolID"] = $school['schoolID'];
     header('Location: in-school.php');
     exit();
 }
 else{
+    #the user is not in a school
     header('Location: no-school.php');
     exit();
 }
