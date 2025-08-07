ec9bd4e11149638e0470ee2481b30cf527697a0d
kaipCS <parker.ceh@oundleschool.org.uk>
2025-08-07 13:42:13 +0100

    write up join school

--- joinschool.php
+++ joinschool.php
@@ -17,24 +17,33 @@ if (empty($_POST["schoolID"])){
 else{
     #id was entered
     $school = $_POST["schoolID"];
+
+    #check school already exists 
     $stmt = $conn->prepare("SELECT COUNT(*) FROM users WHERE schoolID = :schoolID");
     $stmt->bindParam(':schoolID', $school);
     $stmt->execute();
     $count = $stmt->fetchColumn();
     if ($count >0){
+        #avoid malicious attack injecting code
         $school = htmlspecialchars($school);
 
+        #add school id into users table
         $stmt = $conn->prepare("UPDATE users SET schoolID = :schoolID WHERE userid = :userid");
         $stmt->bindParam(':schoolID', $school);
         $stmt->bindParam(':userid', $_SESSION["userid"]);
         $stmt->execute();
 
+        #set school id session variable
         $_SESSION["schoolID"] = $school;
+
+        #redirect to in school page
         header('Location: in-school.php');
         exit();
-}
+    }
     else{
-        $_SESSION["error"] = "noID";  
+        #set session variable to indicate nature of error
+        $_SESSION["error"] = "noID"; 
+        #redirect back to sign in again
         header('Location: no-school.php');
         exit(); 
     }}
